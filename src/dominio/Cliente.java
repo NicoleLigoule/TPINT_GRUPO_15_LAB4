bdbfa2680dc233878e5ef8abf7136b0173baff1c
Nicole <nicole.ligoule@alumnos.frgp.utn.edu.ar>
2024-10-22 00:18:47 -0300

    Esto no lo postee en el foro porq es para entrega 2. En esta entrega SOLO VISTAS

--- src/dominio/Cliente.java
+++ src/dominio/Cliente.java
@@ -1,5 +1,20 @@
 package dominio;
 
 public class Cliente {
-
+    private int idCliente;
+    private int dni;
+    private int cuil;
+    private String nombre;
+    private String apellido;
+    private String sexo;
+    private String nacionalidad;
+    private String fechaNacimiento; // mas adelante seguro modificamos esto
+    private String direccion;
+    private String localidad;
+    private String provincia;
+    private String correoElectronico;
+    private int telefonos;
+    private String usuario;
+    private String contraseña;
+    private boolean esAdmin;
 }
