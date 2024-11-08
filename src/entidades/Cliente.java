afa3574c3c136b9027ea775e64ca2642c6c1f522
Dante_dev <109294689+Dantlu@users.noreply.github.com>
2024-11-08 18:15:28 -0300

    agrego telefono en clase cliente

--- src/entidades/Cliente.java
+++ src/entidades/Cliente.java
@@ -13,6 +13,7 @@ public class Cliente {
 	private String direccion;
 	private int id_localidad;
 	private String correo;
+	private String telefono;
 	private boolean estado;
 	
 	public Cliente() {
@@ -20,7 +21,7 @@ public class Cliente {
 	}
 	
 	public Cliente(String cuil, int dni, String nombre, String apellido, int id_sexo, String id_nacionalidad,
-			LocalDate fechaNacimiento, String direccion, int id_localidad, String correo, boolean estado) {
+			LocalDate fechaNacimiento, String direccion, int id_localidad, String correo, String telefono, boolean estado) {
 		super();
 		this.cuil = cuil;
 		this.dni = dni;
@@ -32,6 +33,7 @@ public class Cliente {
 		this.direccion = direccion;
 		this.id_localidad = id_localidad;
 		this.correo = correo;
+		this.telefono = telefono;
 		this.estado = estado;
 	}
 
@@ -114,6 +116,14 @@ public class Cliente {
 	public void setCorreo(String correo) {
 		this.correo = correo;
 	}
+	
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
 
 	public boolean isEstado() {
 		return estado;
@@ -127,9 +137,11 @@ public class Cliente {
 	public String toString() {
 		return "Cliente [cuil=" + cuil + ", dni=" + dni + ", nombre=" + nombre + ", apellido=" + apellido + ", id_sexo="
 				+ id_sexo + ", id_nacionalidad=" + id_nacionalidad + ", fechaNacimiento=" + fechaNacimiento
-				+ ", direccion=" + direccion + ", id_localidad=" + id_localidad + ", correo=" + correo + ", estado="
-				+ estado + "]";
+				+ ", direccion=" + direccion + ", id_localidad=" + id_localidad + ", correo=" + correo + ", telefono="
+				+ telefono + ", estado=" + estado + "]";
 	}
+
+	
 	
 	
 	
