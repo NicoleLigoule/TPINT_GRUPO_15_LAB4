95bf38b47096de9919c5598e76025870392d1f40
Dante_dev <109294689+Dantlu@users.noreply.github.com>
2024-11-11 16:20:10 -0300

    saco comentarios

--- src/daoImpl/Conexion.java
+++ src/daoImpl/Conexion.java
@@ -6,20 +6,20 @@ import java.sql.SQLException;
 
 public class Conexion {
     private static final String URL = "jdbc:mysql://localhost:3306/bancoutn";
-    private static final String USER = "root"; // Reemplaza con tu usuario
-    private static final String PASSWORD = "root"; // Reemplaza con tu contraseña
+    private static final String USER = "root"; 
+    private static final String PASSWORD = "root"; 
     private static Conexion instance;
     private Connection connection;
 
     private Conexion() {
     	  try {
               Class.forName("com.mysql.jdbc.Driver");
-            //  this.connection = DriverManager.getConnection(host + dbName, user, pass);
+            
           } catch (Exception e) {
               e.printStackTrace();
           }
         try {
-            // Establece la conexión a la base de datos
+           
             this.connection = DriverManager.getConnection(URL, USER, PASSWORD);
             System.out.println("Conexión exitosa");
         } catch (SQLException e) {
