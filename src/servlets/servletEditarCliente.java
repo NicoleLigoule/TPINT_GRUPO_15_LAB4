3469bb46d93b35662acf19a6156a6bec9c66c6cd
danimrios <danimrios09@gmail.com>
2024-11-15 22:47:25 -0300

    Update servletEditarCliente.java

--- src/servlets/servletEditarCliente.java
+++ src/servlets/servletEditarCliente.java
@@ -1,6 +1,8 @@
 package servlets;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
@@ -10,6 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import abml.abmlCliente;
 import entidades.Localidad;
 import entidades.Cliente;
 import entidades.Nacionalidad;
@@ -171,7 +174,85 @@ public class servletEditarCliente extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		 
 		
+		 if(request.getParameter("agregarBtn") != null) {
+	            Cliente c = new Cliente();
+	            
+	            String dni = request.getParameter("dni");
+	            if (dni != null && !dni.isEmpty()) {
+	                c.setDni(Integer.parseInt(dni));
+	            }
+	            System.out.println("Datos recibidos en servletCliente: DNI = " + dni);
+	            
+	            c.setCuil(request.getParameter("cuil"));
+	            System.out.println("CUIL: " + request.getParameter("cuil"));
+	            
+	            c.setNombre(request.getParameter("nombre"));
+	            System.out.println("Nombre: " + request.getParameter("nombre"));
+	            
+	            c.setApellido(request.getParameter("apellido"));
+	            System.out.println("Apellido: " + request.getParameter("apellido")); 
+	            
 
+	            String genero = request.getParameter("genero");
+	            if (genero != null && !genero.isEmpty()) {
+	                c.setId_sexo(Integer.parseInt(genero));
+	                System.out.println("Genero: " + genero);
+	            }
+
+	            String fechaStr = request.getParameter("fecha-nacimiento");
+	            if (fechaStr != null && !fechaStr.isEmpty()) {
+	                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+	                LocalDate fechaNacimiento = LocalDate.parse(fechaStr, formatter);
+	                c.setFechaNacimiento(fechaNacimiento);
+	                System.out.println("FechaNacimiento: " + request.getParameter("fecha-nacimiento"));  
+	            }
+
+	            String nacionalidad = request.getParameter("nacionalidad");
+	            if (nacionalidad != null && !nacionalidad.isEmpty()) {
+	                try {
+	                    c.setId_nacionalidad(nacionalidad);
+	                    System.out.println("Nacionalidad: " + request.getParameter("nacionalidad"));
+	                  
+	                } catch (NumberFormatException e) {
+	                    System.out.println("Error: el valor de nacionalidad no es un número válido.");
+	                    request.setAttribute("Error", "El valor de nacionalidad no es válido.");
+	                    request.getRequestDispatcher("/AgregarCliente.jsp").forward(request, response);
+	                    return;
+	                }
+	            }
+
+	            String localidad = request.getParameter("localidad");
+	            if (localidad != null && !localidad.isEmpty()) {
+	                c.setId_localidad(Integer.parseInt(localidad));
+	                System.out.println("Localidad: " + localidad);
+	            }
+
+	            String provincia = request.getParameter("provincia");
+	            if (provincia != null && !provincia.isEmpty()) {
+	                c.setId_provincia(Integer.parseInt(provincia));
+	                System.out.println("Provincia: " + request.getParameter("provincia"));
+	            }
+
+	            c.setDireccion(request.getParameter("direccion"));
+	            System.out.println("Direccion: " + request.getParameter("direccion"));
+	            
+	            c.setTelefono(request.getParameter("telefono"));
+	            System.out.println("Telefono: " + request.getParameter("telefono"));
+	            
+	            c.setCorreo(request.getParameter("email"));
+	            System.out.println("Email: " + request.getParameter("email"));
+
+	            abmlCliente abmlcliente = new abmlCliente();
+	            boolean insert = abmlcliente.editarCliente(c);
+
+		            if (insert) {         
+		                response.sendRedirect("AgregarCliente.jsp?status=success");
+		            } else {
+		                request.setAttribute("Error", "Error al agregar Cliente.");
+		                RequestDispatcher rd = request.getRequestDispatcher("AgregarCliente.jsp");
+		                rd.forward(request, response);
+		            }   
+	        	}
 		
 	}
 }
